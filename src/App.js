752696c46edb234b4ba85d229ca2dd4706dae4f6
Ever's PC <gomezsantiagoeverardo@gmail.com>
2020-09-09 18:41:59 -0700

    error

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import SearchBar from './Components/SearchBar';
 
 function App() {
+  // Components in blank spaces
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="navBar">
+        <header className="title"><h1>Google Books React Search</h1></header>
+        <SearchBar></SearchBar>
+      </div>
+      <div>
+        <h2>Books Recently Searched</h2>
+        <h2>
+
+        </h2>
+      </div>
     </div>
   );
 }
